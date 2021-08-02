c5520029496d0f066fde816765fbc25d43214a3f
hyuk <yomuto@gmail.com>
2021-08-02 23:47:27 +0900

    page 32

--- cpp_codingtest.cpp
+++ cpp_codingtest.cpp
@@ -1,7 +1,26 @@
 ﻿#include <array>
 #include <iostream>
 
-int main()
+template <size_t N>
+static void print(const std::array<int, N>& arr)
+{
+#if 0
+	for (auto ele : arr)
+	{
+		std::cout << ele << ", ";
+	}
+#else
+	for (auto i = arr.begin(); i != arr.end(); i++)
+	{
+		auto element = (*i);
+		std::cout << element << ", ";
+	}
+#endif
+
+	std::cout << std::endl;
+}
+
+int main(void)
 {
 	std::array<int, 10> arr1;
 
@@ -18,6 +37,26 @@ int main()
 	}
 
 	std::cout << std::endl;
+
+	std::array<int, 4> arr3 = { 2, 4, 6, 7 };
+
+	try
+	{
+		std::cout << arr3.at(3) << std::endl;
+		std::cout << arr3.at(4) << std::endl;
+	}
+	catch (const std::out_of_range& ex)
+	{
+		std::cerr << ex.what() << std::endl;
+	}
+
+	std::array<int, 5> arr4 = { 2, 4, 6, 7, 5 };
+
+	print(arr4);
+
+	std::cout << arr4.front() << std::endl;
+	std::cout << arr4.back() << std::endl;
+	std::cout << *(arr4.data() + 1) << std::endl;
 
 	return 0;
 }
